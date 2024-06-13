ffd8115e923bb6430d5d225ea6d055e6912f354c
PraneetBandi123 <praneetbandi2002@gmail.com>
2024-06-13 19:56:35 -0400

    Finalize registration details display functionality.

--- script.js
+++ script.js
@@ -8,12 +8,33 @@ document.addEventListener("DOMContentLoaded" , (event) =>{
         //Receive inputs from form submission
         const userName=document.getElementById("userName").value;
         const emailAddress=document.getElementById("emailAddress").value;
-        const date=document.getElementById("emailAddress").value;
+        const date=document.getElementById("date").value;
         const veg=document.getElementById("veg").checked;
         const nonVeg=document.getElementById("nonVeg").checked;
-        const vegan=document.getElementById("vegan"). checked ;
+        const vegan=document.getElementById("vegan").checked ;
 
-    }
+        //Dynamically create user registration form elements
+        document.getElementById("userRegistrationSubmission").innerHTML =
+        ` <h3>User Registration Completed!</h3>
+        <b>User Name:</b> ${userName} <br>
+        <b> Email Address:</b> ${emailAddress}<br>
+        <b> Date:</b> ${date} <br>
+        <b>Meal Preferences:</b><br>
+        Vegetarian: ${veg} <br>
+        Non-Vegetarian: ${nonVeg} <br>
+
+        Vegan: ${vegan} <br>`
+
+        //Clears values in the form after submission.
+        document.getElementById("userRegistrationForm").reset();
+
+        
+    
+
+
+
+    })
+})
 
 
 
